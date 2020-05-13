2d51010b7cb73a53fcb194abee3b54ec6243c5b9
lukas-ostrovskis <lukas.ostrovskis1@gmail.com>
2020-05-13 15:17:04 +0300

    Function for processing bmi

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -11,22 +11,27 @@ namespace BootCamp.Chapter
         {
             for (int i = 0; i < 2; i++)
             {
-                string name = PromptString("Name: ");
-
-                string surname = PromptString("Surname: ");
-
-                int age = PromptInt("Age: ");
-
-                float weight = PromptFloat("Weight(kg): ");
-
-                float heightCM = PromptFloat("Height(cm): ");
-                float heightM = heightCM / 100.0f;
-
-                Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + heightCM + " cm.");
-
-                float BMI = CalculateBmi(weight, heightM);
-                Console.WriteLine("His Body-Mass Index(BMI) is " + BMI);
+                ProcessBmi();
             }
+        }
+
+        public static void ProcessBmi()
+        {
+            string name = PromptString("Name: ");
+
+            string surname = PromptString("Surname: ");
+
+            int age = PromptInt("Age: ");
+
+            float weight = PromptFloat("Weight(kg): ");
+
+            float heightCM = PromptFloat("Height(cm): ");
+            float heightM = heightCM / 100.0f;
+
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + heightCM + " cm.");
+
+            float BMI = CalculateBmi(weight, heightM);
+            Console.WriteLine("His Body-Mass Index(BMI) is " + BMI);
         }
 
         public static string PromptString(string message)
